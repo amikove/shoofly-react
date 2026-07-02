25d46fdb69f574a5dceda7cd9c25e798ece45215
amikove <amikove@gmail.com>
2026-07-02 20:26:24 +0100

    Centralize VILLES/quartiers data into shared constants file (manual edit, no PowerShell rewrite)

--- src/pages/oeil/Missions.jsx
+++ src/pages/oeil/Missions.jsx
@@ -1,6 +1,7 @@
 import ChatModal from '../../components/missions/ChatModal'
 import { useState, useEffect, useCallback } from 'react'
 import AppLayout from '../../components/layout/AppLayout'
+import { VILLES } from '../../constants/villes'
 import Topbar from '../../components/layout/Topbar'
 import { missionsAPI, reportsAPI } from '../../api'
 import { StatusBadge, Spinner, EmptyState, toast, Pagination } from '../../components/ui'
@@ -18,27 +19,7 @@ const TABS = [
   { id: 'done',      label: 'Terminées'    },
 ]
 const TYPE_ICONS = { immobilier:'🏠', file_attente:'⏳', audit:'🔎', personnalisee:'🎯' }
-const VILLES = {
-  'Rabat': ['Agdal','Hassan','Océan','Souissi','Aviation','Hay Riad','Youssoufia','Akkari','Diour Jamaa','Médina','Orangers','Ryad','Centre Ville','Hay Nahda','Hay Fadoul','Takaddoum','Hay Karima','Hay Salama','Mabella','Hay Ennakhil'],
-  'Salé': ['Bettana','Hay Salam','Tabriquet','Laâyoune','Médina','Kébibat','Hssaine','Sidi Moussa','Bab Lamrissa','Hay Karima','Hay Arrahma','Hay Al Majd','Attacharouk','Hay Essalam','Layayda','Ouled Mtaa','Hay Nasr','Sidi Taibi','Hay Al Wifaq','Hay Al Massira'],
-  'Témara': ['Hay Al Fath','Massira','Oumassa','Centre Ville','Ain Attig','Menzeh','Hay Farah','Hay Al Amal','Hay Essalam','Hay Ennour','Hay Nakhil','Résidence Al Wifaq','Hay Nahda','Hay Rihane','Sidi Yahya','Mansouria'],
-  'Casablanca': ['Maarif','Bourgogne','Gauthier','Hay Hassani','Ain Chock','Ain Sebaa','Anfa','Bernoussi','Bouskoura','CIL','Californie','Derb Sultan','Hay Mohammadi','Moulay Rachid','Sidi Belyout','Sbata','Médina','Oulfa','Ben M\'sick','Sidi Bernoussi','Roches Noires','Polo','Racine','Val Fleuri','Riviera','Hay Al Farah','Hay Ennakhil','Hay Inara','Lissasfa','Lahraouiyine','Sidi Maarouf','Dar Bouazza','Belvédère','Palmier','Sidi Othmane','Hay El Hana','Zenata','Tit Mellil','Hay Moulay Abdallah','Sidi Moumen','Nassim'],
-  'Marrakech': ['Guéliz','Hivernage','Médina','Mellah','Daoudiate','Massira','Syba','M\'hamid','Targa','Azzouzia','Palmeraie','Hay Charaf','Douar Lahna','Sidi Youssef Ben Ali','Bab Doukkala','Ménara','Hay Al Majd','Tamansourt','Iziki','Hay Hassani','Hay Mohammadi','Mouassine','Bab Ghmat','Kennaria','Riad Zitoun','Arset El Maach','Hay Houta','Résidence Al Wifaq'],
-  'Fès': ['Médina','Ville Nouvelle','Jdid','Saïss','Narjiss','Bensouda','Aouinat Hajjaj','Zouagha','Hay Amal','Hay Mahatta','Atlas','Agdal','Hay Nakhil','Sidi Brahim','Hay Essalam','Ain Chkef','Hay Wifaq','Hay Massira','Dokkarat','Hay Moulay Slimane','Hay Ennahda','Hay Karima','Oued Fès','Hay Al Farah','Ain Kadous'],
-  'Meknès': ['Hamria','Médina','Ville Nouvelle','Ismaïlia','Marjane','Bassatine','Hay Salam','Hay Nour','Hay Wifaq','Hay Amal','Hay Inara','Résidence Ismaïlia','Hay Al Majd','Hay Doum','Riad','Hay Mansour','Hay Zitoune','Ain Smen','Borj Moulay Omar','Hay Karima'],
-  'Tanger': ['Médina','Malabata','Marshan','Gzenaya','Mesnana','Iberia','California','Achakar','Boukhalef','Hay Karima','Bni Makada','Hay Amal','Dradeb','Ain Ktiouet','Hay Nakhil','Moujahidine','Val Fleuri','Hay Almohades','Hay Nahda','Rmilat','Souani','Branes','Hay Hassani','Hay Mohammadi','Charf','Tanger City Center'],
-  'Agadir': ['Hay Mohammadi','Talborjt','Dakhla','Charaf','Bensergao','Anza','Tilila','Founty','Hay Salam','Hay Amal','Hay Al Massira','Tikiouine','Hay Hassani','Adrar','Cité Suisse','Résidence Yasmina','Hay Yassmine','Aourir','Hay Nahda','Hay Al Wifaq','Ait Melloul','Inezgane'],
-  'Oujda': ['Médina','Lazaret','Sidi Maâfa','Hay Al Qods','Isly','Université','Hay Salam','Hay Amal','Hay Nakhil','Hay Wifaq','Ain Sfa','Hay Essalam','Hay Al Majd','Hay Riad','Sidi Ziane','Hay Karima','Hay Mohammadi','Hay Andalous','Ennakhil'],
-  'Kénitra': ['Médina','Hay Mahatta','Hay Salam','Bir Rami','Saknia','Hay Amal','Hay Nakhil','Hay Wifaq','Hay Karima','Centre Ville','Hay Nassim','Hay El Wahda','Hay Hassani','Maamoura','Hay Mohammadi','Hay Al Farah','Résidence Al Wifaq','Hay Essalam'],
-  'Tétouan': ['Médina','Ensanche','Martil','Mhannech','Azla','Hay Salam','Hay Amal','Hay Nahda','Hay Nakhil','Hay Wifaq','Hay Karima','Dersa','Touabel','Sidi Mandri','Hay Al Majd','Hay Mohammadi','Hay Hassani','Résidence Al Farah','Ain Lhout','Hay Riad'],
-  'Mohammedia': ['Centre Ville','Ain Harrouda','Sidi Moumen','Médina','Hay Hassani','Hay Mohammadi','Hay Salam','Hay Amal','Hay Wifaq','Hay Nakhil','Hay Karima','Hay Al Majd','Hay Nassim','Sidi Maarouf','Hay Essalam','Résidence Al Wifaq','Hay Nahda','Ain Sbiaa'],
-  'El Jadida': ['Médina','Hay Hassani','Azemmour','Centre Ville','Hay Salam','Hay Amal','Hay Mohammadi','Hay Wifaq','Hay Nakhil','Hay Karima','Hay Al Majd','Haouzia','Hay Essalam','Bir Jdid','Résidence Al Farah','Hay Nahda','Sidi Bouzid'],
-  'Safi': ['Médina','Hay El Amal','Arsat Lhamra','Centre Ville','Hay Hassani','Hay Mohammadi','Hay Salam','Hay Wifaq','Hay Nakhil','Hay Karima','Hay Al Majd','Hay Essalam','Hay Nahda','Résidence Al Wifaq','Sidi Bouzid','Hay Inara','Jrifat'],
-  'Béni Mellal': ['Centre Ville','Hay Amal','Oulad Yaïch','Médina','Hay Hassani','Hay Salam','Hay Mohammadi','Hay Wifaq','Hay Nakhil','Hay Karima','Hay Al Majd','Hay Essalam','Hay Nahda','Taboun','Ain Asserdoun','Résidence Al Farah','Hay Inara','Hay Riad'],
-  'Nador': ['Centre Ville','Médina','Hay Salam','Hay Amal','Hay Nakhil','Hay Wifaq','Hay Karima','Hay Mohammadi','Hay Hassani','Beni Chiker','Hay Al Majd','Hay Nahda','Hay Essalam','Hay Riad','Résidence Al Wifaq','Azghangan'],
-  'Settat': ['Centre Ville','Hay Salam','Hay Amal','Hay Nakhil','Hay Wifaq','Hay Karima','Hay Mohammadi','Hay Hassani','Hay Al Majd','Hay Nahda','Hay Essalam','Médina','Résidence Al Farah','Hay Riad'],
-  'Laâyoune': ['Centre Ville','Hay Salam','Hay Amal','Hay Nakhil','Hay Wifaq','Hay Karima','Hay Mohammadi','Hay Hassani','Hay Al Majd','Hay Nahda','Hay Essalam','Cité Militaire','Résidence Al Wifaq','Hay Riad'],
-}
+
 
 export default function OeilMissions() {
   const [complianceMission, setComplianceMission] = useState(null)
